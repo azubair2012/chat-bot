722b42dd4060698437926a11f9322730e6a1ebf1
azubair2012 <studio.zubair@gamil.com>
2023-10-02 16:19:23 +0600

    fc

--- app/layout.js
+++ app/layout.js
@@ -5,14 +5,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Chat Bot",
-    description: "Full functional chat bot using ChatGPT",
+    description: "Fully functional chat bot using ChatGPT",
 };
 
 export default function RootLayout({ children }) {
     return ( <
         html lang = "en" >
         <
-        body className = { inter.className } > { children } < /body> <
+        body className = { inter.className } > { children } < /body>{" "} <
         /html>
     );
 }
